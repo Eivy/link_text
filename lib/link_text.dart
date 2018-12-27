e52b8fdb0b694e860aeb141301bf63f6dd383f13
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-12-27 15:58:57 +0900

    Handle GestureRecognizer lifecycle

--- lib/link_text.dart
+++ lib/link_text.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class LinkText extends StatelessWidget {
+class LinkText extends StatefulWidget {
   LinkText(this.text, {this.linkStyle, this.textStyle});
 
   final String text;
@@ -13,21 +13,29 @@ class LinkText extends StatelessWidget {
   final TextStyle textStyle;
 
   @override
+  createState() => LinkTextState();
+}
+
+class LinkTextState extends State<LinkText> {
+  List<GestureRecognizer> recList = new List<GestureRecognizer>();
+
+  @override
   Widget build(BuildContext context) {
 
-    var text = Theme.of(context).textTheme.body1.merge(textStyle);
+    var text = Theme.of(context).textTheme.body1.merge(widget.textStyle);
     var link = Theme.of(context).textTheme.body1
         .merge(TextStyle(inherit: true, color: Colors.blue, decoration: TextDecoration.underline))
-        .merge(linkStyle);
+        .merge(widget.linkStyle);
 
     var children = new List<TextSpan>();
-    var matches = RegExp(r'https?://[a-zA-Z0-9\-%_/=&?.]+').allMatches(this.text).toList();
+    var matches = RegExp(r'https?://[a-zA-Z0-9\-%_/=&?.]+').allMatches(widget.text).toList();
     var links = matches.map<TextSpan>((m) {
       var rec = TapGestureRecognizer()..onTap = () async {
         if (await canLaunch(m.group(0))) {
           await launch(m.group(0));
         }
       };
+      recList.add(rec);
       return TextSpan(text: m.group(0), style: link, recognizer: rec);
     }
     ).toList();
@@ -35,23 +43,31 @@ class LinkText extends StatelessWidget {
       for (var i = 0; i < matches.length; i++) {
         if (i == 0) {
           if (matches[i].start != 0) {
-            children.add(TextSpan(text: this.text.substring(0, matches[i].start), style: text));
+            children.add(TextSpan(text: widget.text.substring(0, matches[i].start), style: text));
           }
           children.add(links[i]);
           continue;
         }
         children.add(
-            TextSpan(text: this.text.substring(matches[i - 1].end, matches[i].start), style: text));
+            TextSpan(text: widget.text.substring(matches[i - 1].end, matches[i].start), style: text));
         children.add(links[i]);
       }
-      if (matches.last.end != this.text.length) {
-        children.add(TextSpan(text: this.text.substring(matches.last.end),
+      if (matches.last.end != widget.text.length) {
+        children.add(TextSpan(text: widget.text.substring(matches.last.end),
                 style: text));
       }
     } else {
-      children.add(TextSpan(text: this.text, style: text));
+      children.add(TextSpan(text: widget.text, style: text));
     }
     return RichText(text: TextSpan(children: children));
+  }
+
+  @override
+  dispose() {
+    for (var i = recList.length-1; i >= 0; i--) {
+      recList.removeAt(i);
+    }
+    super.dispose();
   }
 
 }
